f8ca1f35305182deef44c4b92ed19bc704b069f9
Mohammed Alshehri <542855+dralshehri@users.noreply.github.com>
2025-10-22 01:13:08 +0300

    Update to n8n 1.116.2

--- overrides/packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.vue
+++ overrides/packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.vue
@@ -72,15 +72,15 @@ onMounted(() => {
 }
 
 .logoText {
-	margin-left: var(--spacing-5xs);
+	margin-left: var(--spacing--5xs);
 	path {
-		fill: var(--color-text-dark);
+		fill: var(--color--text--shade-1);
 	}
 }
 
 .large {
 	transform: scale(1.2);
-	margin-bottom: var(--spacing-xl);
+	margin-bottom: var(--spacing--xl);
 
 	.logo,
 	.logoText {
@@ -88,13 +88,13 @@ onMounted(() => {
 	}
 
 	.logoText {
-		margin-left: var(--spacing-xs);
-		margin-right: var(--spacing-3xs);
+		margin-left: var(--spacing--xs);
+		margin-right: var(--spacing--3xs);
 	}
 }
 
 .sidebarExpanded .logo {
-	margin-left: var(--spacing-2xs);
+	margin-left: var(--spacing--2xs);
 }
 
 .sidebarCollapsed .logo {
